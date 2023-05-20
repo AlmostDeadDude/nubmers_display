fbd71f3092e3a656533ada0a454809c96388fd1e
Ivan Shiller <ivan.shiller@protonmail.com>
2023-05-20 17:55:54 +0200

    adjusted height for mobile browsers

--- index.php
+++ index.php
@@ -21,38 +21,40 @@ $numbers = explode(',', $numbers);
 </head>
 
 <body>
-    <header>
-        <div id="timeInput" class="inputGroup">
-            <label for="time">Time step [s]</label>
-            <input type="number" id="time" name="time" min="2" max="20" value="2">
-        </div>
-        <div id="progress" class="inputGroup">
-            <label for="progress_step">Current progress</label>
-            <div style="width: 100%; position: relative;">
-                <input type="number" name="progress_step" id="progress_step">
-                <span id="progress_total">/</span>
-                <button id="goToStep">
-                    <i class="fa-solid fa-arrow-right"></i>
-                </button>
+    <div class="wrapper">
+        <header>
+            <div id="timeInput" class="inputGroup">
+                <label for="time">Time step [s]</label>
+                <input type="number" id="time" name="time" min="2" max="20" value="2">
             </div>
+            <div id="progress" class="inputGroup">
+                <label for="progress_step">Current progress</label>
+                <div style="width: 100%; position: relative;">
+                    <input type="number" name="progress_step" id="progress_step">
+                    <span id="progress_total">/</span>
+                    <button id="goToStep">
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </button>
+                </div>
+            </div>
+        </header>
+        <main id="a">
+            <div class="num num-n4"><span></span></div>
+            <div class="num num-n3"><span></span></div>
+            <div class="num num-n2"><span></span></div>
+            <div class="num num-n1"><span></span></div>
+            <div class="num num-0"><span>0</span></div>
+            <div class="num num-p1"><span>...</span></div>
+            <div class="num num-p2"><span>...</span></div>
+            <div class="num num-p3"><span>...</span></div>
+            <div class="num num-p4"><span>...</span></div>
+        </main>
+        <div class="buttons">
+            <button id="btn-start"><i class="fa-solid fa-play"></i></button>
+            <button id="btn-stop"><i class="fa-solid fa-stop"></i></button>
         </div>
-    </header>
-    <main id="a">
-        <div class="num num-n4"><span></span></div>
-        <div class="num num-n3"><span></span></div>
-        <div class="num num-n2"><span></span></div>
-        <div class="num num-n1"><span></span></div>
-        <div class="num num-0"><span>0</span></div>
-        <div class="num num-p1"><span>...</span></div>
-        <div class="num num-p2"><span>...</span></div>
-        <div class="num num-p3"><span>...</span></div>
-        <div class="num num-p4"><span>...</span></div>
-    </main>
-    <div class="buttons">
-        <button id="btn-start"><i class="fa-solid fa-play"></i></button>
-        <button id="btn-stop"><i class="fa-solid fa-stop"></i></button>
+        <div id="circle"></div>
     </div>
-    <div id="circle"></div>
     <script>
         <?php
         //we pass the numbers to the javascript
